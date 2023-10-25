69ba36cf09c1a88627a0c40799b6ff267c08153d
casualtimetravler <125123956+casualtimetravler@users.noreply.github.com>
2023-10-25 09:20:20 +0300

    Update index.php

--- newWeb/lab/index.php
+++ newWeb/lab/index.php
@@ -17,6 +17,42 @@
         echo mysqli_error($con);
         }
         mysqli_close($con);
+ //if (mysqli_query($con, $sql)) {
+    //    echo "New record created successfully";
+     //   } else {
+     //   echo mysqli_error($con);
+     //   }
+     //   mysqli_close($con);
+    
+    $sql= "SELECT * from students limit 1, 2";
+    $result = mysqli_query($con, $sql);
+    if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+            echo " - Name: ".$row["name"]."<br>";
+            }
+     } 
+    else {
+           echo "No records";
+    }
+    $sql = "DELETE FROM students WHERE id=3";
+
+    if (mysqli_query($con, $sql)) {
+        echo "Record deleted successfully";
+    }
+    else {
+        echo mysqli_error($con);
+    }
+ 
+    mysqli_close($con);
+
+    //$sql = "UPDATE students SET name='lanya' WHERE id=2";
+
+    //if (mysqli_query($con, $sql)) {
+    // echo "Record updated successfully";
+    //} 
+    //else {
+    //  echo mysqli_error($con);
+    // }
     ?>
 </body>
 </html>
